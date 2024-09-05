7cd4b5f2223d5253d126d1117ab0c35c770c9994
priyanka.kalavapalli <priyanka.kalavapalli@AVD-P-90ee>
2024-09-04 23:18:58 -0700

    Add project files.

---
+++ CheckDbApi/Controllers/CheckController.cs
@@ -0,0 +1,32 @@
+﻿using CheckDbApi.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CheckDbApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CheckController : ControllerBase
+	{
+		private readonly CheckdbContext _context;
+        public CheckController(CheckdbContext context)
+        {
+			_context = context;
+            
+        }
+		[HttpGet("check-db-connection")]
+		public async Task<IActionResult> CheckDbConnection()
+		{
+			try
+			{
+				await _context.Database.ExecuteSqlRawAsync("SELECT 1");
+				return Ok("Db Connection successful");
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500,$"Db Connection failed: {ex.Message}");
+			}
+		}
+	}
+}
---
+++ CheckDbApi/Data/CheckdbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace CheckDbApi.Data
+{
+	public class CheckdbContext:DbContext
+	{
+        public CheckdbContext(DbContextOptions<CheckdbContext> options): base(options) 
+        {
+            
+        }
+    }
+}
